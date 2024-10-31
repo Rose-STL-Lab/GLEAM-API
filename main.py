9e2dd5d97d26958d14f6e77676dd1068b88646da
CodingKomodo <ecao7777@gmail.com>
2024-10-31 11:24:27 -0700

    finished prototype api key auth

--- main.py
+++ main.py
@@ -1,47 +1,10 @@
 import numpy as np
 import json
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, FastAPI
 from pydantic import BaseModel
-from datetime import datetime, timedelta, timezone
-from jose import JWTError, jwt
-from passlib.context import CryptContext
 from seir import seir, full_seir    
 from os import environ
-from dotenv import load_dotenv
-
-load_dotenv()
-
-SECRET_KEY = environ['SECRET_KEY']
-ALGORITHM = environ['ALGORITHM']
-ACCESS_TOKEN_EXPIRE_MINUTES = environ['ACCESS_TOKEN_EXPIRE_MINUTES']
-
-fake_db = {
-    "ethan": {
-        "username": "ethan",
-        "full_name": "Ethan Cao",
-        "email": "etcao@ucsd.edu",
-        "hashed_password": "",
-        "disabled": False
-        
-    }
-}
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    
-class TokenData(BaseModel):
-    username: str | None = None
-    
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-    
-class UserInDB(User):
-    hashed_password: str
+from auth import get_user
 
 class Params(BaseModel):
     days: int
@@ -54,82 +17,27 @@ class ListParams(BaseModel):
     sims: int
     beta_epsilon: list
     
-    
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
             return obj.tolist()
         return super().default(obj)
     
-pwd_context = CryptContext(schemes=['bcrypt'],deprecated = 'auto')
-oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-    
 app = FastAPI()
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-def get_user(db, username: str):
-    if username in db:
-        user_data = db[username]
-        return UserInDB(**user_data)
-    
-def authenticate_user(db, username:str, password:str):
-    user = get_user(db,username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-        
-    to_encode.update({'exp':expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm = ALGORITHM)
-    return encoded_jwt
-
-async def get_current_user(token: str = Depends(oauth_2_scheme)):
-    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail = "Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
-    
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credential_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credential_exception
-    user = get_user(fake_db, username=token_data.username)
-    if user is None:
-        raise credential_exception
-    return user
-
-async def get_current_active_user(current_user:UserInDB = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
 @app.get("/")
-def test():
-    return
+def test(user: dict = Depends(get_user)):
+    return user
 
 @app.post("/original")
-def original(params: Params):
+def original(params: Params, user:dict = Depends(get_user)):
     item_1, item_2, item_3 = seir(params.days,params.beta, params.epsilon,params.sims)
     json_dump = json.dumps({"train_set": item_1}, 
                        cls=NumpyEncoder)
     return json_dump
 
 @app.post("/multiple")
-def multiple(params: ListParams):
+def multiple(params: ListParams, user:dict = Depends(get_user)):
     beta_epsilon = np.array(params.beta_epsilon)
     
     item_1 = full_seir(params.days,beta_epsilon,params.sims)
