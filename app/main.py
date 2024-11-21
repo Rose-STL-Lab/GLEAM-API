a5e0079fc5df205864647e12f80b13ffff8c284f
CodingKomodo <ecao7777@gmail.com>
2024-11-21 13:16:25 -0800

    added compute engine code

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from pydantic import BaseModel
 from app.seir import seir, full_seir    
 from os import environ
 from app.auth import get_user
-from google.cloud.firestore_v1 import DocumentReference
+from google.cloud.firestore_v1 import DocumentReference, DocumentSnapshot
+from compute import create_instance_with_docker
 
 class Params(BaseModel):
     days: int
@@ -17,6 +18,12 @@ class ListParams(BaseModel):
     days: int
     sims: int
     beta_epsilon: list
+
+class ComputeParams(BaseModel):
+    days: int
+    sims: int
+    beta: float
+    epsilon: float
     
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -27,23 +34,41 @@ class NumpyEncoder(json.JSONEncoder):
 app = FastAPI()
 
 @app.get("/")
-def test(user = Depends(get_user)):
+def test(user: tuple[DocumentSnapshot, DocumentReference] = Depends(get_user)):
     num_runs = user[0].to_dict()['Num_Runs_This_Month']
     user[1].update({'Num_Runs_This_Month': num_runs + 1})
     return user[0].to_dict()
 
 @app.post("/original")
-def original(params: Params, user = Depends(get_user)):
+def original(params: Params, user: tuple[DocumentSnapshot, DocumentReference] = Depends(get_user)):
     item_1, item_2, item_3 = seir(params.days,params.beta, params.epsilon,params.sims)
     json_dump = json.dumps({"train_set": item_1}, 
                        cls=NumpyEncoder)
     return json_dump
 
 @app.post("/multiple")
-def multiple(params: ListParams, user = Depends(get_user)):
+def multiple(params: ListParams, user: tuple[DocumentSnapshot, DocumentReference] = Depends(get_user)):
     beta_epsilon = np.array(params.beta_epsilon)
     
     item_1 = full_seir(params.days,beta_epsilon,params.sims)
     json_dump = json.dumps({"train_set": item_1}, 
                        cls=NumpyEncoder)
     return json_dump
+
+@app.post("/create_compute")
+def create_compute(user: tuple[DocumentSnapshot, DocumentReference] = Depends(get_user)):
+    output = create_instance_with_docker(
+        project_id="epistorm-gleam-api",
+        zone="us-central1-a",
+        instance_name="my-docker-vm8",
+        machine_type="e2-medium",
+        image_family="debian-12",
+        image_project="debian-cloud",
+        docker_image="gcr.io/epistorm-gleam-api/seir",
+        beta=2,
+        epsilon=0.5,
+        simulations=30,
+        days=100,
+        bucket='seir-output-bucket-2'
+        )
+    return
