9faa34d46a88c4803877632b4e822839fe3aa0a5
CodingKomodo <ecao7777@gmail.com>
2024-12-05 14:23:22 -0800

    fixed timestamp

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from os import environ
 from app.auth import get_user
 from google.cloud.firestore_v1 import DocumentReference, DocumentSnapshot
 from app.compute import create_instance_with_docker
-import datetime
+import time
 
 class Params(BaseModel):
     days: int
@@ -58,7 +58,7 @@ def multiple(params: ListParams, user: tuple[DocumentSnapshot, DocumentReference
 
 @app.post("/create_compute")
 def create_compute(params: Params, user: tuple[DocumentSnapshot, DocumentReference] = Depends(get_user)):
-    timestamp = str(datetime.datetime.now())
+    timestamp = str(int(time.time()))
     output = create_instance_with_docker(
         project_id="epistorm-gleam-api",
         zone="us-central1-a",
