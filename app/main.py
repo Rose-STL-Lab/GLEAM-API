a1762521c8987bc49a300ae2b7accf3e3c508f1d
LiamManatt <lmanatt@ucsd.edu>
2025-02-10 19:32:48 -0800

    adding julia endpoint (testing)

--- app/main.py
+++ app/main.py
@@ -285,4 +285,28 @@ def create_image(params: ComputeWithConfig, user: tuple[DocumentSnapshot, Docume
     return timestamp
 
 
+@app.post("/julia_create_image")
+def julia_create_image(params: CreateImageParams,
+                 background_tasks: BackgroundTasks, 
+    user: tuple[DocumentSnapshot, DocumentReference] = Depends(get_user)):
+
+    timestamp = str(int(time.time()))
+
+
+    background_tasks.add_task(
+        create_instance_and_save_image,
+        project_id="epistorm-gleam-api",
+        zone="us-central1-a",
+        instance_name=f"image-generator-{timestamp}",
+        machine_type="e2-medium",
+        image_family="debian-12",
+        image_project="debian-cloud",
+        bucket_name=params.bucket_name,
+        folder_name=params.folder_name,
+        project_name=params.requirements_name,
+        custom_image_name=params.image_name + "-" + timestamp,
+    )
+    return params.image_name + "-"+ timestamp
+
+
 #just a test
